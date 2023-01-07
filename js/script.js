3438a5bafc7e210e99eed761f9480b29f05e5372
“Gilson <gilson.maran@gmail.com>
2023-01-07 20:49:22 -0300

    Exibir resultado. Scripts movidos para arquivo externo. Adicao de logotipo.

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
+// ---------------------------
 // CALCULAR IDADE
-
-
+// ---------------------------
 const dateBirth = document.getElementById('date-birth')
 const dateExam = document.getElementById('date-exam')
 const age = document.getElementById('age')
@@ -9,6 +9,8 @@ dateBirth.addEventListener('change', () => {
     calculateAge(dateExam.value, age);
 });
 
+
+dateExam.value = moment().format('YYYY-MM-DD')
 dateExam.addEventListener('change', () => {
     calculateAge(dateExam.value, age);
 });
@@ -26,4 +28,85 @@ function calculateAge(dataConsulta, alvo) {
     const meses = parseInt((idadeReal - anos) * 12)
 
     alvo.value = `${anos}a ${meses}m`
+}
+
+// ---------------------------
+// ADICIONAR AS IMAGENS
+// ---------------------------
+const images = [
+    'img-1',
+    'img-2',
+    'img-3',
+    'img-4',
+];
+
+images.forEach(image => {
+    const element = document.getElementById(image)
+
+    element.addEventListener('change', () => {
+        const id = image.split('-')[1]
+        const target = document.getElementById(`exam-${id}`)
+        const file = element.files[0];
+
+        if (file)
+            target.src = URL.createObjectURL(file);
+    })
+})
+
+// ---------------------------
+// CALCULAR SCORE
+// ---------------------------
+let score = 0
+const result = document.getElementById('result')
+const inputs = [...document.querySelectorAll('input[type="radio"]')]
+
+inputs.forEach(e => {
+    e.addEventListener('click', () => {
+        score = 0
+
+        inputs.forEach(input => {
+            if (input.checked)
+                score += parseInt(input.value)
+        })
+
+        showResult()
+
+        result.innerText = score === 1
+            ? `${score} ponto`
+            : `${score} pontos`
+    })
+})
+
+// ---------------------------
+// RESULTADO
+// ---------------------------
+resetResults()
+
+function resetResults() {
+    const results = ['result1', 'result2', 'result3']
+    results.forEach(r => {
+        document.getElementById(r).style.display = 'none'
+        document.getElementById(r).style.display = 'none'
+        document.getElementById(r).style.display = 'none'
+    })
+}
+
+function showResult() {
+    resetResults()
+
+    if (score <= 3) {
+        document.getElementById('result1').style.display = 'block';
+        return;
+    }
+
+    if (score <= 7) {
+        document.getElementById('result2').style.display = 'block';
+        return;
+    }
+
+    if (score < 99) {
+        document.getElementById('result3').style.display = 'block';
+        return;
+    }
+
 }
